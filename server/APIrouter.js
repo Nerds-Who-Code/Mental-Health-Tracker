8246c9ebf167044a86c916c0a2e65004d97b9e7e
steph-koopmanschap <stephkoopmanschap@protonmail.com>
2022-07-12 20:22:49 +0800

    Wrapped all routes in try-catch blocks

--- server/APIrouter.js
+++ server/APIrouter.js
@@ -54,6 +54,8 @@ function APIlogger(req, res, next) {
 //Mount the APIlogger middleware to the APIrouter
 APIrouter.use(APIlogger);
 
+// All routes should be wrapped up in try - catch blocks to prevent the entire server from crashing upon errors.
+
 // =============USER FUNCTIONS==================
 
 //req.params.username is the last part in the URL (:username) 
@@ -61,44 +63,63 @@ APIrouter.use(APIlogger);
 //https://localhost:3001/getUser/test123
 //req.params.username = "test123"
 APIrouter.get('/getUser/:username', (req, res, next) => {
-    const foundUser = getUser(req.params.username);
-    if (!(foundUser instanceof Error)) {
-        //User is found. Send the user data.
-        res.status(200).send(foundUser);
+    try {
+        const foundUser = getUser(req.params.username);
+        if (!(foundUser instanceof Error)) {
+            res.status(200).send(foundUser);
+        }
+        // User not found error. Send 404 response.
+        res.status(404).send(foundUser.message);
+    // Internal server errors
+    } catch (error) {
+        console.log("500: Internal server error - " + error.message);
+        res.status(500).send(error.message);
     }
-    //User not found error. Send 404 response.
-    res.status(404).send(foundUser.message);
 });
 
 APIrouter.get('/getUserById/:id', (req, res, next) => {
-    const foundUser = getUserById(req.params.id);
-    if (!(foundUser instanceof Error)) {
-        res.status(200).send(foundUser);
+    try {
+        const foundUser = getUserById(req.params.id);
+        if (!(foundUser instanceof Error)) {
+            res.status(200).send(foundUser);
+        }
+        res.status(404).send(foundUser.message);
+    } catch (error) {
+        console.log("500: Internal server error - " + error.message);
+        res.status(500).send(error.message);
     }
-    res.status(404).send(foundUser.message);
 });
 
 //We are using a PUT request instead of a GET request so that the user can
 //send the password in the request body for secure (HTTPS) encyrption instead of through the URL.
 //This way the password is not exposed in plain text.
 APIrouter.put('/getUserByPasswd/:username', (req, res, next) => {
-    const foundUser = getUserByPasswd(req.params.username, req.body.password);
-    if (!(foundUser instanceof Error)) {
-        res.status(201).send(foundUser);
+    try {
+        const foundUser = getUserByPasswd(req.params.username, req.body.password);
+        if (!(foundUser instanceof Error)) {
+            res.status(201).send(foundUser);
+        }
+        res.status(404).send(foundUser.message);
+    } catch (error) {
+        console.log("500: Internal server error - " + error.message);
+        res.status(500).send(error.message);
     }
-    res.status(404).send(foundUser.message);
 });
 
-APIrouter.put('/loginUser/:username', (req, res, next) => {
-    //We need to wrap this in a self-executing async function, because the hashing library is also async.
-    (async () => {
+//This route is async  because the hashing library is also async.
+APIrouter.put('/loginUser/:username', async (req, res, next) => {
+    try {
+        //We need to wrap this in a self-executing async function, because the hashing library is also async.
         const foundUser = await loginUser(req.params.username, req.body.password);
         if (!(foundUser instanceof Error)) {
             res.status(201).res.send(foundUser);
         } else {
             res.status(404).send(foundUser.message);
         }
-    })();
+    } catch (error) {
+        console.log("500: Internal server error - " + error.message);
+        res.status(500).send(error.message);
+    }
 });
 
 //JSON Request Body Input:
@@ -111,34 +132,57 @@ For example if you want to change the age of a user to 5, the request body becom
  "value": 5}
 */
 APIrouter.put('/updateUser/:username', (req, res, next) => {
-    const updatedUser = updateUser(req.params.username, req.body.property, req.body.value);
-    if (!(updatedUser instanceof Error)) {
-        res.status(201).res.send(foundUser);
+    try {
+        const updatedUser = updateUser(req.params.username, req.body.property, req.body.value);
+        if (!(updatedUser instanceof Error)) {
+            res.status(201).res.send(foundUser);
+        }
+        res.status(404).send(updatedUser.message);
+    } catch (error) {
+        console.log("500: Internal server error - " + error.message);
+        res.status(500).send(error.message);
     }
-    res.status(404).send(updatedUser.message);
 });
 
 /**@todo Validate the data with Express-Validator */
 //JSON Request Body Input:
 //{"user:" {your_user_data_here}}
-APIrouter.post('/createUser/:username', (req, res, next) => {
-    //We need to wrap this in a self-executing async function, because the hashing library is also async.
-    (async () => {
-        /*  There is a bug here when: if there 2 create requests made one after another, both with the exact same data,
-            there will be an error and the server shuts down.
-            Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client
-        */
+//This route is async  because the hashing library is also async.
+APIrouter.post('/createUser/:username', 
+               [],
+               async (req, res, next) => {
+    /*  There is a bug here when: if there 2 create requests made one after another, both with the exact same data,
+        there will be an error and the server shuts down.
+        Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client
+    */
+
+    // Check for body errors (Data validation)
+    const validationErrors = validationResult(req);
+    if(!validationErrors.isEmpty()) {
+        // Send Bad Request Response with the error codes of the validation.
+        return res.status(400).send({errors: validationErrors.array()});
+    }
+
+    try {
         const createdUser = await createUser(req.params.username, req.body.user);
         if (!(createUser instanceof Error)) {
             res.status(201).send(createdUser);
         }
         res.status(404).send(createdUser.message);
-    })();
+    } catch (error) {
+        console.log("500: Internal server error - " + error.message);
+        res.status(500).send(error.message);
+    }
 });
 
 APIrouter.delete('/deleteUser/:username', (req, res, next) => {
-    deleteUser(req.params.username);
-    res.status(201).send(`User with username ${req.params.username} was deleted.`);
+    try {
+        deleteUser(req.params.username);
+        res.status(201).send(`User with username ${req.params.username} was deleted.`);
+    } catch (error) {
+        console.log("500: Internal server error - " + error.message);
+        res.status(500).send(error.message);
+    }
 });
 
 // =============ENTRY FUNCTIONS==================
@@ -146,11 +190,16 @@ APIrouter.delete('/deleteUser/:username', (req, res, next) => {
 //JSON Request Body Input:
 //{"entry:" {your_entry_data_here}}
 APIrouter.post('/addEntry/:username', (req, res, next) => {
-    const createdEntry = addEntry(req.params.username, req.body.entry);
-    if (!(createdEntry instanceof Error)) {
-        res.status(201).send(createdEntry);
+    try {
+        const createdEntry = addEntry(req.params.username, req.body.entry);
+        if (!(createdEntry instanceof Error)) {
+            res.status(201).send(createdEntry);
+        }
+        res.status(404).send(createdEntry.message);
+    } catch (error) {
+        console.log("500: Internal server error - " + error.message);
+        res.status(500).send(error.message);
     }
-    res.status(404).send(createdEntry.message);
 });
 
 //JSON Request Body Input:
@@ -163,21 +212,29 @@ For example if you want to change the level of an entry to 6, the request body b
  "value": 6}
 */
 APIrouter.put('/updateEntry/:username/:entryID', (req, res, next) => {
-    const updatedEntry = updateEntry(req.params.username, req.params.entryID, req.body.property, req.body.value);
-    if (!(updatedEntry instanceof Error)) {
-        res.status(201).send(updatedEntry);
+    try {
+        const updatedEntry = updateEntry(req.params.username, req.params.entryID, req.body.property, req.body.value);
+        if (!(updatedEntry instanceof Error)) {
+            res.status(201).send(updatedEntry);
+        }
+        res.status(404).send(updatedEntry.message);
+    } catch (error) {
+        console.log("500: Internal server error - " + error.message);
+        res.status(500).send(error.message);
     }
-    res.status(404).send(updatedEntry.message);
-
-
 });
 
 APIrouter.delete('/deleteEntry/:username/:entryID', (req, res, next) => {
-    const isError = deleteEntry(req.params.username, req.params.entryID);
-    if (!(isError instanceof Error)) {
-        res.status(201).send(`Entry with entry ID: ${req.params.entryID} was deleted.`);
+    try {
+        const isError = deleteEntry(req.params.username, req.params.entryID);
+        if (!(isError instanceof Error)) {
+            res.status(201).send(`Entry with entry ID: ${req.params.entryID} was deleted.`);
+        }
+        res.status(404).send(isError);
+    } catch (error) {
+        console.log("500: Internal server error - " + error.message);
+        res.status(500).send(error.message);  
     }
-    res.status(404).send(isError);
 });
 
 module.exports = APIrouter;
