5fa3e0082daa274a2ebf2f3bab465df9c8996b8b
steph-koopmanschap <stephkoopmanschap@protonmail.com>
2022-07-20 14:04:13 +0800

    Fixed the bug to show all the events in Entry.js

--- client/src/components/Entry.js
+++ client/src/components/Entry.js
@@ -13,6 +13,7 @@ export default function Entry(props) {
     };
 
     return (
+
         <div className="px-4 py-4 odd:bg-white even:bg-blue-100" >
             
                 <span className="entryDate inline-block w-28 px-2">{props.entryInfo.date}</span>
@@ -25,7 +26,9 @@ export default function Entry(props) {
                 <button className="entryEditButton inline-block w-6 px-2">✏️</button>
                 <button className="entryDeleteButton inline-block w-8 px-2" onClick={deleteEntryOnClick}>🗑️</button>
                  
+
         </div>
     )
 }
+
 
