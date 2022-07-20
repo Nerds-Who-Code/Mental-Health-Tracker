3804dca387943d0fc01da44350c139e6a52df91f
steph-koopmanschap <stephkoopmanschap@protonmail.com>
2022-07-20 14:04:13 +0800

    Fixed the bug to show all the events in Entry.js

--- client/src/components/AddEntryPage.js
+++ client/src/components/AddEntryPage.js
@@ -3,9 +3,13 @@ import {useDispatch, useSelector} from 'react-redux';
 import {Link, useNavigate} from "react-router-dom";
 import {addEntry} from '../store';
 
+// This is used to temporally store the events of the entry before submit.
+const events = [];
+
 export default function AddEntryPage() {
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    // Get the username from the global state.
     const username = useSelector(state => state.userData.userInfo.username);
 
     // Values of all the input boxes
@@ -13,16 +17,99 @@ export default function AddEntryPage() {
     const [entryState, setEntryState] = useState({
         type: "Stress",
         level: 1,
-        event: "work",
+        event: [],
         note: "Your note here..."
     });
 
-    // Handles changes in the input boxes (Saves user input to the React State manager)
-    // e is the event that is accociated with the input box that the user is inputting/using
-    // make sure to keep the name attribute of html element the same as the key in state object
-    const handleChange = (e) => {
-        //const value = evt.target.type === "checkbox" ? e.target.checked : e.target.value;
+    // A boolean array for checking the click or unclicked state of the events checkboxes
+    const [isCheckedEventsArray, setIsCheckedEventsArray] = useState(
+        new Array(6).fill(false)
+    );
+
+    /*
+     Handles changes in the input boxes (Saves user input to the React State manager)
+     e is the event that is accociated with the input box that the user is inputting/using
+     make sure to keep the name attribute of html element the same as the key in state object
+     checkedPosition is the index of the checkboxes
+    */
+    const handleChange = (e, checkedPosition=0) => {
+        
+        // Handle the checkboxes.
+        if (e.target.name === "event") {
+            // Checks or unchecks each checkbox and maps it to an array of booleans.
+            const updatedIsCheckedEventsArray = isCheckedEventsArray.map((checkBoxState, index) =>
+                index === checkedPosition ? !checkBoxState : checkBoxState
+            );
+            setIsCheckedEventsArray(updatedIsCheckedEventsArray);
+            
+            //let indexAttr = e.target.getAttribute('index');
+
+            console.log("==================================");
+            console.log("BEFORE");
+            console.log(updatedIsCheckedEventsArray)
+            console.log(e.target.name);
+            console.log("events:");
+            console.log(events);
+            //console.log(indexAttr);
+            //console.log(parseInt(indexAttr));
+
+            // Transforms the array of booleans of the checkboxes to an array with values for each event.
+            // checkBoxState is equal to the value of each item of boolean array
+            // The index is equal to the index of the boolean array.
+            updatedIsCheckedEventsArray.forEach( (checkBoxState, index) => {
+                // Explicitly checking for true value to not make myself confused.
+                // same as if(checkBoxState) {}
+                console.log("index: " + index);
+                if (checkBoxState === true) {
+                    // Each index corresponds to the checked or unchecked state of each of the 6 checkboxes.
+                    switch(index) {
+                        case 0:
+                            events[0] = "family";
+                            break;
+                        case 1:
+                            events[1] = "relationship";
+                            break;
+                        case 2:
+                            events[2] = "work";
+                            break;
+                        case 3:
+                            events[3] = "significant";
+                            break;
+                        case 4:
+                            events[4] = "trauma";
+                            break;
+                        case 5:
+                            events[5] = "unknown";
+                            break;
+                        default: 
+                            return 0;
+                    }
+                }
+                else {
+                    events[index] = null;
+                }
+            });
+
+            console.log("==================================");
+            console.log("AFTER");
+            console.log(updatedIsCheckedEventsArray)
+            console.log(e.target.name);
+            console.log("events:");
+            console.log(events);
+            
+            // Set the event variable equal to the events array.
+            setEntryState({
+                ...entryState,
+                [e.target.name]: events
+            });
+
+            // Escape the function with no errors 
+            // This is to prevent the state from being written over by the next setEentryState below this code
+            return 0;
+        } 
+        // Get the value of the input boxes.
         const value = e.target.value;
+        // This is used to set the value of all the other input boxes except the checkboxes.
         setEntryState({
             ...entryState,
             [e.target.name]: value
@@ -56,9 +143,12 @@ export default function AddEntryPage() {
     return (
         <div className="flex flex-col items-center justify-center my-16">
             <h1 className="my-2">{entryState.type} Tracker</h1>
-            <form id="addEntry" className="flex flex-col items-left justify-center  bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" action="#" onSubmit={addNewEntry}>
-                
-                <div >
+            <form 
+                id="addEntry" 
+                className="flex flex-col items-left justify-center  bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" 
+                action="#" 
+                onSubmit={addNewEntry}>
+                <div>
                     <div className="radioInput">
                         
                         <input 
@@ -73,7 +163,7 @@ export default function AddEntryPage() {
                     </div>
                     
                     <div className="radioInput">
-                       
+                    
                         <input
                             id="type-anxiety"
                             type="radio"
@@ -82,7 +172,7 @@ export default function AddEntryPage() {
                             checked={entryState.type === "Anxiety"}
                             onChange={handleChange}
                         />
-                         <label forhtml="type-anxiety">Anxiety</label>
+                        <label forhtml="type-anxiety">Anxiety</label>
                     </div>
 
                     <div className="radioInput">
@@ -121,67 +211,79 @@ export default function AddEntryPage() {
                 
                 <div className="checkboxInput">
                 <input 
+                    index="0"
                     type="checkbox"
                     name="event" 
                     value="family"
-                    onChange={handleChange} 
+                    checked={isCheckedEventsArray[0]}
+                    onChange={(e) => handleChange(e, 0)} 
                 />
                 <label>Family problem</label>
                 </div>
 
             <div className="checkboxInput">
                 <input 
+                    index="1"
                     type="checkbox"
                     name="event" 
                     value="relationship"
-                    onChange={handleChange} 
+                    checked={isCheckedEventsArray[1]}
+                    onChange={(e) => handleChange(e, 1)}  
                 />
                 <label>Relationship problem</label>
             </div> 
 
             <div className="checkboxInput">
                 <input 
+                    index="2"
                     type="checkbox"
                     name="event" 
                     value="work"
-                    onChange={handleChange} 
+                    checked={isCheckedEventsArray[2]}
+                    onChange={(e) => handleChange(e, 2)}
                 />
                 <label>School or work</label>
-             </div>
+            </div>
 
-             <div className="checkboxInput">
+            <div className="checkboxInput">
                 <input 
+                    index="3"
                     type="checkbox"
                     name="event" 
                     value="significant"
-                    onChange={handleChange} 
+                    checked={isCheckedEventsArray[3]}
+                    onChange={(e) => handleChange(e, 3)}
                 />
                 <label>Significant life event</label>
             </div>
 
             <div className="checkboxInput">
                 <input 
+                    index="4"
                     type="checkbox"
                     name="event" 
                     value="trauma"
-                    onChange={handleChange} 
+                    checked={isCheckedEventsArray[4]}
+                    onChange={(e) => handleChange(e, 4)}
                 />
                 <label>A traumatic event</label>
                 </div> 
 
                 <div className="checkboxInput">
                 <input 
+                    index="5"
                     type="checkbox"
                     name="event" 
                     value="unknown"
-                    onChange={handleChange} 
+                    checked={isCheckedEventsArray[5]}
+                    onChange={(e) => handleChange(e, 5)}
                 />
                 <label>I am not sure</label>
                 </div>
                 
                 <label>Notes for today's entry (optional):</label>
 
-               <div className="flex flex-col items-center">
+                <div className="flex flex-col items-center">
                 <input className="self-stretch m-2 p-1"
                     
                     type="textarea"
@@ -189,7 +291,7 @@ export default function AddEntryPage() {
                     value={entryState.note}
                     onChange={handleChange} 
                 />
-             
+            
                 <input className="my-2 mx-2 px-4 py-2 leading-none  rounded text-white  hover:text-white bg-gradient-to-r from-green-300 to-blue-400 hover:from-cyan-200 hover:to-green-300 "
                     type="submit"
                     value="Add entry" 
--- client/src/components/Entry.js
+++ client/src/components/Entry.js
@@ -12,25 +12,21 @@ export default function Entry(props) {
         dispatch(deleteEntry({username: username, entryId: props.entryInfo.entryId}));
     };
 
-    /*  
-        There is a bug here where if the events are more than 1,
-        it displays "Unknown"
-    */
-
     return (
-        <div className="px-4 py-4 odd:bg-white even:bg-blue-100" >
-            
-                <span className="entryDate inline-block w-28 px-2">{props.entryInfo.date}</span>
-                
-                
-                <span className="entryLevel inline-block w-6 px-2">{props.entryInfo.level}</span>
-                <span className="entryType inline-block w-32 px-2">{props.entryInfo.type}</span>
-                <span className="entryEvent inline-block w-48 px-2">{props.entryInfo.event}</span>
-                <span className="entryNoteHover inline-block w-6 px-2">📔<div className="hidden inline">{props.entryInfo.notes}</div></span>
-                <button className="entryEditButton inline-block w-6 px-2">✏️</button>
-                <button className="entryDeleteButton inline-block w-8 px-2" onClick={deleteEntryOnClick}>🗑️</button>
-                 
+        <div className="entryCard">
+            <h4>Date Created: {props.entryInfo.date}</h4>
+            <hr />
+            <p><strong>Type: </strong>{props.entryInfo.type}</p>
+            <p><strong>Level: </strong>{props.entryInfo.level}</p>
+            <p><strong>Events: </strong> 
+            {props.entryInfo.event.filter(item => item !== null).map((item, index) => 
+                <span key={index}>{`${item}, `}</span>)}
+            </p>
+            <p><strong>Notes: </strong>{props.entryInfo.notes}</p>
+            <button>Edit</button>
+            <button onClick={deleteEntryOnClick}>Delete</button>
         </div>
     )
 }
+
 
