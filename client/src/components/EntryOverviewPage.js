6c975afee7cee7612d8d49385ed1cf477a93d560
steph-koopmanschap <stephkoopmanschap@protonmail.com>
2022-10-05 08:51:24 +0800

    testing-chartjs-entry-overview

--- client/src/components/EntryOverviewPage.js
+++ client/src/components/EntryOverviewPage.js
@@ -1,14 +1,54 @@
+import React from 'react';
 import {Link} from "react-router-dom";
+import Chart from "chart.js/auto";
+import {Line, Bar, Pie} from "react-chartjs-2";
+import ErrorBoundary from './ErrorBoundary';
 
-export default function EntryOverViewPage() {
+const labels = ["January", "February", "March", "April", "May", "June"];
 
-    let entries = "";
+const data = {    
+  labels: labels,
+  datasets: [
+    {
+      label: "My First dataset",
+      backgroundColor: "rgb(255, 99, 132)",
+      borderColor: "rgb(255, 99, 132)",
+      data: [0, 10, 5, 2, 20, 30, 45],
+    },
+  ],
+};
+
+export default function EntryOverviewPage() {
 
     return (
         <div>
-            <h1>Your Mental Health Overview</h1>
-            <div>{entries}</div>
-            <Link to="/dashboard"><button>Go back to dashboard</button></Link>
+            <div className='flex flex-col items-center justify-center   '>
+                <h1
+                  className='m-2'>
+                  Your Mental Health Overview</h1>
+                <Link to="/dashboard">
+                  <button 
+                    className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none mx-2'>
+                    Go back to dashboard
+                  </button>
+                </Link>
+            </div>
+            <div>
+            <ErrorBoundary>
+                <Bar data={data} />
+              </ErrorBoundary>
+              <ErrorBoundary>
+                <Pie data={data} />
+              </ErrorBoundary>
+              <ErrorBoundary>
+                <Line data={data} />
+              </ErrorBoundary>
+            </div>
         </div>
-    )
+    );
 }
+//<Line data={data} />
+
+{/* <ErrorBoundary>
+  <LineChart />
+</ErrorBoundary> */}
